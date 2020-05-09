425ba324a6742ec70d92d95e79dddb5add00342f
coadingBus <1138848222@qq.com>
2020-05-10 00:44:43 +0800

    测试2

---
+++ src/main/java/com/example/demo/e/Test.java
@@ -0,0 +1,11 @@
+package com.example.demo.e;
+
+/**
+ * @author gengdebao
+ * @date 2020-05-10 0:43
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(555);
+    }
+}
